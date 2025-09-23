1be120451abcc48b446b7caf1a4cee552e9ef95b
Rohit041118 <rohitkhond2004@gmail.com>
2025-09-23 19:17:02 +0530

    Day 4 Progress...

---
+++ script5.js
@@ -0,0 +1,38 @@
+//--Functions           //Prevents redundancy.
+//Block of code performs specific task, can be invoked whenever needed.
+//Function Def - 
+//function funcName(param1, param2, ....){
+//     body
+//}
+//Function Call - 
+//functionName();
+
+function rk(){
+    console.log("He is RK...");
+    console.log("Nope i am RK...");
+}
+rk();    //function invoked.
+// rk();
+
+function rks(msg){
+    console.log(msg);
+}
+rks(msg);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
